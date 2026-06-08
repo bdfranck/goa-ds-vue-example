665ef64ca052634be10c620c5dde1432a4d4fbcd
Benji Franck <benjamin.franck@gmail.com>
2026-06-08 17:59:35 -0600

    Add example Public Form page

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AgeVerificationStep from "./components/AgeVerificationStep.vue";
+</script>
+
+<template>
+  <goa-one-column-layout>
+    <section slot="header">
+      <goa-app-header version="2" url="#" heading="Apply for a service">
+        <a slot="utilities" href="#">Sign in</a>
+      </goa-app-header>
+    </section>
+
+    <goa-page-block width="704px">
+      <AgeVerificationStep />
+    </goa-page-block>
+
+    <section slot="footer">
+      <goa-app-footer version="2"></goa-app-footer>
+    </section>
+  </goa-one-column-layout>
+</template>
---
+++ src/components/AgeVerificationStep.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface DatePickerChangeDetail {
+  value?: string;
+}
+
+const birthday = ref("");
+const error = ref(false);
+
+const onBirthdayChange = (event: Event): void => {
+  const customEvent = event as CustomEvent<DatePickerChangeDetail>;
+  birthday.value = customEvent.detail?.value ?? "";
+  error.value = false;
+};
+
+const handleSubmit = (): void => {
+  if (!birthday.value) {
+    error.value = true;
+  }
+};
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <goa-link
+      version="2"
+      href="#"
+      color="secondary"
+      leadingicon="arrow-back"
+      size="small"
+      mt="xl"
+      mb="2xl"
+    >
+      Back
+    </goa-link>
+
+    <goa-text as="div" size="body-l" mt="none" mb="xs"
+      >Verify your age</goa-text
+    >
+    <goa-text as="h2" mt="none" mb="xl">What is your date of birth?</goa-text>
+
+    <goa-callout
+      v-if="error"
+      version="2"
+      type="emergency"
+      heading="There is a problem"
+      emphasis="low"
+      mb="xl"
+    >
+      <ul>
+        <li>Enter your date of birth</li>
+      </ul>
+    </goa-callout>
+
+    <goa-form-item
+      version="2"
+      for="birthday"
+      :error="error ? 'Enter your date of birth' : undefined"
+    >
+      <goa-date-picker
+        id="birthday"
+        version="2"
+        name="birthday"
+        type="input"
+        @_change="onBirthdayChange"
+        :error="error"
+      />
+    </goa-form-item>
+
+    <goa-button
+      version="2"
+      type="primary"
+      mt="xl"
+      mb="none"
+      @click="handleSubmit"
+    >
+      Continue
+    </goa-button>
+  </form>
+</template>
+
+<style scoped>
+ul {
+  margin: 0;
+}
+</style>
