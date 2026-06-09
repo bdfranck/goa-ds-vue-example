3bc1298edba7dce59ca095135c83dd91808a85b1
Benji Franck <benjamin.franck@gmail.com>
2026-06-08 18:10:20 -0600

    Fix GitHub Pages issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue({
+      
       template: {
         compilerOptions: {
           isCustomElement: (tag) => tag.startsWith('goa-'),
@@ -12,4 +13,5 @@ export default defineConfig({
       },
     }),
   ],
+  base: '/goa-ds-vue-example/',
 })
